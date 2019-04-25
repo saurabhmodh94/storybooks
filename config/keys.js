f9fb47ff4760ad3d7e57cc3f1a5f7db9ddf3116e
Saurabh Modh <saurabhmodh94@gmail.com>
2019-04-25 22:46:02 +0530

    remove keys

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mongoURI:
-    'mongodb+srv://admin:test123@cluster0-cgbuo.mongodb.net/storybooks?retryWrites=true',
-  googleClientID:
-    '50918386748-uiscrs4tvjeevu0ta93fav00aao8ikkl.apps.googleusercontent.com',
-  googleClientSecret: 's8-fXiMGYpUZ87SN2AAsmQgC',
-  callbackURL: '/auth/google/callback'
-};
