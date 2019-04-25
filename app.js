aa729860057ec786ab46e35ccdb86c00ea29b23b
Saurabh Modh <saurabhmodh94@gmail.com>
2019-04-25 10:36:42 +0530

    basic passport config

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 
 const app = express();
+
+// Passport Config
+require('./config/passport')(passport);
+
+// Load Routes
+const auth = require('./routes/auth');
 
 // Index Route
 app.get('/', (req, res) => {
   res.send('index');
 });
+
+// Use Routes
+app.use('/auth', auth);
 
 const port = process.env.PORT || 5000;
 
