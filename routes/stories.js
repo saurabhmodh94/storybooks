58fd41ad50d38dff987e49e97793f3dbfa6b9c5b
Saurabh Modh <saurabhmodh94@gmail.com>
2019-04-25 18:22:48 +0530

    basic Add Story

--- routes/stories.js
+++ routes/stories.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
 const { ensureAuthenticated, ensureGuest } = require('../helpers/auth');
+
+const Story = mongoose.model('stories');
 
 // Stories Index
 router.get('/', (req, res) => {
@@ -10,6 +13,22 @@ router.get('/', (req, res) => {
 // Add Story Form
 router.get('/add', ensureAuthenticated, (req, res) => {
   res.render('stories/add');
+});
+
+// Process Add Story
+router.post('/', (req, res) => {
+  const newStory = {
+    title: req.body.title,
+    body: req.body.body,
+    status: req.body.status,
+    allowComments: req.body.allowComments ? true : false, // tip
+    user: req.user.id
+  };
+
+  // Create Story
+  new Story(newStory).save().then(story => {
+    res.redirect(`/stories/show/${story.id}`);
+  });
 });
 
 module.exports = router;
