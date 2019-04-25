fcd9f4166fb059719ff3617ca0c7f16360f8597e
Saurabh Modh <saurabhmodh94@gmail.com>
2019-04-25 22:51:22 +0530

    keys config: dev, prod

---
+++ config/keys.prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: process.env.CALLBACK_URL
+};
